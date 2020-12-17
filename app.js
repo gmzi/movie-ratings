8ca4cdd0d1a7b1ca610937d3e9b3ba75f0b63fa8
gmzi <gastonmazieres@gmail.com>
2020-12-17 16:18:46 -0500

    index and style done

--- app.js
+++ app.js
@@ -5,10 +5,14 @@ const $btnSubmit = $('input[type="submit"]');
 $btnSubmit.on('click', function (e) {
   e.preventDefault();
   let $newTitle = $('#title').val();
-  let $newRating = $('#rating').val();
-  let $newMovie = `<li class="new-title">${$newTitle.toUpperCase()}. <span class="new-rating">Rated: ${$newRating} stars</span><span class="remove"><button id="remove-btn">Remove</button></span></li>`;
-  $('ul').append($newMovie);
-  $('ul #remove-btn').on('click', function () {
-    $(this).parent().parent().remove();
-  });
+  const $stars = $('input[type="radio"]').get();
+  console.log($stars);
+  console.log('hi');
+
+  //   let $newRating = $('#rating').val();
+  //   let $newMovie = `<li class="new-title">${$newTitle.toUpperCase()}. <span class="new-rating">Rated: ${$newRating} stars</span><span class="remove"><button id="remove-btn">Remove</button></span></li>`;
+  //   $('ul').append($newMovie);
+  //   $('ul #remove-btn').on('click', function () {
+  //     $(this).parent().parent().remove();
+  //   });
 });
