dec5f0dbdddf00492ca47851504b9eba1a2d4dcf
gmzi <gastonmazieres@gmail.com>
2020-12-17 16:41:37 -0500

    capture number of rating

--- app.js
+++ app.js
@@ -6,13 +6,33 @@ $btnSubmit.on('click', function (e) {
   e.preventDefault();
   let $newTitle = $('#title').val();
   const $stars = $('input[type="radio"]').get();
-  console.log($stars);
-  console.log('hi');
+  const newRating = () => {
+    for (let i = 0; i < $stars.length; i++) {
+      if ($stars[i].checked) {
+        console.log($stars[i]);
+        return $stars[i].value;
+      }
+    }
+  };
 
-  //   let $newRating = $('#rating').val();
-  //   let $newMovie = `<li class="new-title">${$newTitle.toUpperCase()}. <span class="new-rating">Rated: ${$newRating} stars</span><span class="remove"><button id="remove-btn">Remove</button></span></li>`;
-  //   $('ul').append($newMovie);
-  //   $('ul #remove-btn').on('click', function () {
-  //     $(this).parent().parent().remove();
-  //   });
+  let $newMovie = `<li class="new-title">${$newTitle.toUpperCase()}. 
+  <span class="new-rating">Rated: ${newRating()} 
+  stars</span><span class="remove">
+  <button id="remove-btn">Remove</button></span></li>`;
+  $('ul').append($newMovie);
+  $('ul #remove-btn').on('click', function () {
+    $(this).parent().parent().remove();
+  });
 });
+
+function findValue(arr) {
+  console.log('vamos');
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].checked) {
+      console.log(arr[i]);
+      return arr[i].value;
+    }
+    console.log('listo');
+    return arr[i];
+  }
+}
